f731b1dcb3a15221d56c67d3ba03fea93c72b508
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-02-12 20:44:45 -0500

    1.14backport

--- src/main/java/com/tfar/randomblock/RandomBlocksSurfaceBuilder.java
+++ src/main/java/com/tfar/randomblock/RandomBlocksSurfaceBuilder.java
@@ -6,16 +6,12 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.Tag;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
-import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 import java.util.Random;
 import java.util.function.Function;
@@ -27,16 +23,16 @@ public class RandomBlocksSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderCon
 
   @Override
   public void buildSurface(Random random, IChunk chunkIn, Biome biomeIn, int x, int z, int startHeight, double noise, BlockState defaultBlock, BlockState defaultFluid, int seaLevel, long seed, SurfaceBuilderConfig config) {
-    this.buildSurface(random, chunkIn, x, z, startHeight);
+    this.buildSurface(chunkIn, x, z, startHeight);
   }
 
-  protected void buildSurface(Random random, IChunk chunkIn, int x, int z, int startHeight) {
-    BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
+  protected void buildSurface(IChunk chunkIn, int x, int z, int startHeight) {
+    BlockPos.MutableBlockPos blockpos$mutable = new BlockPos.MutableBlockPos();
     int k = x & 15;
     int l = z & 15;
     for(int i1 = startHeight; i1 >= 0; --i1) {
       //setPos
-      blockpos$mutable.func_181079_c(k, i1, l);
+      blockpos$mutable.setPos(k, i1, l);
       BlockState blockstate2 = chunkIn.getBlockState(blockpos$mutable);
       if (!blockstate2.isAir()
               && !(blockstate2.getBlock() instanceof FlowingFluidBlock)
